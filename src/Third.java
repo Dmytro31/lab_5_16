0e00dc5f8fefe8d736a1bb62e6b8bc198a5c9eae
Dmytro31 <99250554+Dmytro31@users.noreply.github.com>
2022-05-25 19:36:39 +0300

    Update Third.java

--- src/Third.java
+++ src/Third.java
@@ -1,14 +1,14 @@
 
 public class Third {
     public static void main(String[] args){
-        int prog;
-        prog = (int) (Math.random()*102);
-        if(prog <= 9){
-            System.out.println(prog + " Одна");
-        }else if(prog <= 99){
-            System.out.println(prog + " Дві");
+        int number;
+        number = (int) (Math.random()*102);
+        System.out.println(number);
+        if (0 < number &&  number  < 9){
+            System.out.println("Один знак");
+        }else if (10 < number && number < 99){
+            System.out.println("Складається з двох знаків");
         }else {
-            System.out.println(prog + " Три");
+            System.out.println("Складається з трьох знаків");
         }
-    }
 }
