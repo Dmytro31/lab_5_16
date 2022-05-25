d5cf3b1626225077f3c6a26edd9677c03587d68c
Dmytro31 <99250554+Dmytro31@users.noreply.github.com>
2022-05-25 19:38:32 +0300

    Update Others.java

--- src/Others.java
+++ src/Others.java
@@ -3,21 +3,17 @@ import java.util.Scanner;
 public class Others {
     public static void main(String[] args){
         Scanner can = new Scanner(System.in);
-        System.out.println("Введіть першу Гіпотинузу:");
+        System.out.println("Введіть  Гіпотинузу:");
         int p1 = can.nextInt();
-        System.out.println("Введіть другу стророну: ");
-        int p2 = can.nextInt();
-        System.out.println("Введіть третю стророну: ");
-        int p3 = can.nextInt();
+        int p2 = 3;
+        int p3 = 4;
         if(3==p2 && 4==p3 ){
-            System.out.println("P = "+(p1+p2+p3));
-            System.out.println("S = "+ (p2*p3)*0.5);
-        }else if(4==p2 && 3==p3){
             System.out.println("P = "+(p1+p2+p3));
             System.out.println("S = "+ (p2*p3)*0.5);
         }else {
             System.out.println("Введено не існуюче значення !");
         }
+
     }
 }
 
